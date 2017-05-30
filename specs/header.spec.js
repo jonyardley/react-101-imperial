33302f7d95d0ec6cb58a834dda3758eaee1b292e
Jon Yardley <jon.yardley@red-badger.com>
2017-05-30 20:46:11 +0100

    Add notes to spec

--- specs/header.spec.js
+++ specs/header.spec.js
@@ -5,8 +5,10 @@ import Header from '../components/header.js';
 
 it('render name', () => {
   const props = { name : 'Jon' };
+
   const component = shallow(
     <Header {...props} />
   );
+
   expect(component.find('h1').text()).toEqual('Hello Jon!');
 });
--- specs/todo-list.spex.js
+++ specs/todo-list.spex.js
@@ -13,17 +13,17 @@ it('add an item to the list', () => {
   const input = component.find('input');
   const button = component.find('button');
 
+  // Add one item
   input.simulate('change', { target: { value: 'My first test item' } });
   button.simulate('click');
 
-  expect(input.text()).toEqual('');
-
+  // Add second item
   input.simulate('change', { target: { value: 'My second test item' } });
   button.simulate('click');
 
   const itemList = component.find('li');
 
-  expect(itemList.length).toEqual(2);
-  expect(itemList.at(0).text()).toEqual('My first test item');
-  expect(itemList.at(1).text()).toEqual('My second test item');
+  expect(itemList.length).toEqual(0);
+  expect(itemList.at(0).text()).toEqual('');
+  expect(itemList.at(1).text()).toEqual('');
 });
